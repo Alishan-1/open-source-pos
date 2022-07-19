1874148e51d4de0c8f995f27c2e2eff1fe5fcda7
ali shan <alishan1@live.com>
2022-07-19 10:56:03 +0500

    removed extra showcase from routing

--- open-source-pos-frontend/src/app/app-routing.module.ts
+++ open-source-pos-frontend/src/app/app-routing.module.ts
@@ -39,36 +39,36 @@ const routes: Routes = [
         loadChildren: () =>
           import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule)
       },
-      {
-        path: 'theme',
-        loadChildren: () =>
-          import('./views/theme/theme.module').then((m) => m.ThemeModule)
-      },
-      {
-        path: 'base',
-        loadChildren: () =>
-          import('./views/base/base.module').then((m) => m.BaseModule)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () =>
-          import('./views/buttons/buttons.module').then((m) => m.ButtonsModule)
-      },
-      {
-        path: 'forms',
-        loadChildren: () =>
-          import('./views/forms/forms.module').then((m) => m.CoreUIFormsModule)
-      },
-      {
-        path: 'charts',
-        loadChildren: () =>
-          import('./views/charts/charts.module').then((m) => m.ChartsModule)
-      },
-      {
-        path: 'icons',
-        loadChildren: () =>
-          import('./views/icons/icons.module').then((m) => m.IconsModule)
-      },
+      // {
+      //   path: 'theme',
+      //   loadChildren: () =>
+      //     import('./views/theme/theme.module').then((m) => m.ThemeModule)
+      // },
+      // {
+      //   path: 'base',
+      //   loadChildren: () =>
+      //     import('./views/base/base.module').then((m) => m.BaseModule)
+      // },
+      // {
+      //   path: 'buttons',
+      //   loadChildren: () =>
+      //     import('./views/buttons/buttons.module').then((m) => m.ButtonsModule)
+      // },
+      // {
+      //   path: 'forms',
+      //   loadChildren: () =>
+      //     import('./views/forms/forms.module').then((m) => m.CoreUIFormsModule)
+      // },
+      // {
+      //   path: 'charts',
+      //   loadChildren: () =>
+      //     import('./views/charts/charts.module').then((m) => m.ChartsModule)
+      // },
+      // {
+      //   path: 'icons',
+      //   loadChildren: () =>
+      //     import('./views/icons/icons.module').then((m) => m.IconsModule)
+      // },
       {
         path: 'notifications',
         loadChildren: () =>
